28aa496e785a4622e2676bb3910071ec231002de
parkjoohyung0826 <dochipjh@gmail.com>
2023-12-19 23:07:57 +0900

    merge

---
+++ src/pages/registration/registration.jsx
@@ -0,0 +1,30 @@
+// Registration.jsx
+
+import React from "react";
+import volunmateImg from "../../assets/Logo/volunmate.svg";
+import signinImg from "../../assets/button/signIn.svg";
+import backImg from "../../assets/button/back.svg";
+import "./registration.css"; // CSS 파일 import
+
+function Registration() {
+
+
+    return (
+        <div className="container">
+            <img src={backImg} alt="Back Img" className="back-img" />
+            <img src={volunmateImg} alt="Volunmate Logo" className="volunmate-img" />
+            <p className="styled-paragraph">이름을 입력해주세요</p>
+            <input type="text" placeholder="전체 이름 입력" />
+            <p className="styled-paragraph">카카오톡 아이디를 입력해주세요</p>
+            <input type="text" placeholder="본인의 카카오톡 아이디만 작성" />
+            <img src={signinImg} alt="Signin Logo" />
+        </div>
+    );
+}
+
+export default Registration;
+
+
+
+
+
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,18 @@
 import GlobalLayout from "./pages/_layout";
 import RecruitHome from "./pages/Recruit/RecruitHome";
+import LoginPage from "./pages/login/login"
+import Registration from "./pages/registration/registration";
+import LoginError from "./pages/loginError/loginError";
 
 export const routes = [
   {
     path: "/",
     element: <GlobalLayout />,
     children: [
-      { path: "/", element: <RecruitHome /> }, // 팀원모집 페이지
+      { path: "/loginError", element: <LoginError /> },
+      { path: "/registration", element: <Registration /> },
+      { path: "/", element: <LoginPage /> }, // 팀원모집 페이지
+      { path: "/recruitHome", element: <RecruitHome /> },
     ],
   },
 ];
