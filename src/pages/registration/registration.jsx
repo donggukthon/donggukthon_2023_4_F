a8c63073fa87ae7f1852b262eaf9e7900adcbea5
parkjoohyung0826 <dochipjh@gmail.com>
2023-12-20 07:34:15 +0900

    feat: login구현

--- src/pages/registration/registration.jsx
+++ src/pages/registration/registration.jsx
@@ -4,6 +4,9 @@ import React from "react";
 import volunmateImg from "../../assets/Logo/volunmate.svg";
 import signinImg from "../../assets/button/signIn.svg";
 import backImg from "../../assets/button/back.svg";
+import crtLocImg from "../../assets/button/currentLocation.svg";
+import retryButton from "../../assets/button/retry.svg";
+import locationIcon from "../../assets/Icon/location2.svg";
 import "./registration.css"; // CSS 파일 import
 
 function Registration() {
@@ -16,6 +19,14 @@ function Registration() {
             <input type="text" placeholder="전체 이름 입력" />
             <p className="styled-paragraph">카카오톡 아이디를 입력해주세요</p>
             <input type="text" placeholder="본인의 카카오톡 아이디만 작성" />
+            <p className="styled-paragraph">위치를 설정해주세요</p>
+            <p className="stmt">내 주변에서 봉사를 실천할 수 있을 거예요</p>
+            <img src={crtLocImg} alt="Location Set Button"></img>
+
+            <p className="styled-paragraph">이 주소가 맞나요?</p>
+            <img src={retryButton}></img>
+            <div className="address">
+                <img src={locationIcon}></img>서울특별시 중구 장충동 1가</div>
             <img src={signinImg} alt="Signin Logo" />
         </div>
     );
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,12 +3,14 @@ import RecruitHome from "./pages/Recruit/RecruitHome";
 import LoginPage from "./pages/login/login"
 import Registration from "./pages/registration/registration";
 import LoginError from "./pages/loginError/loginError";
+import MyV_main from "./pages/myVolunteering/main/main";
 
 export const routes = [
   {
     path: "/",
     element: <GlobalLayout />,
     children: [
+      { path: "/myVolunteer_main", element: <MyV_main /> },
       { path: "/loginError", element: <LoginError /> },
       { path: "/registration", element: <Registration /> },
       { path: "/", element: <LoginPage /> }, // 팀원모집 페이지
