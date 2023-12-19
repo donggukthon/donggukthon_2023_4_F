44f38eac1c8623c634a82c162e0200ed2da6f2c1
leechangjoon <2018110021@dongguk.edu>
2023-12-19 20:25:43 +0900

    feat: add 사진등록 폼

--- src/pages/Recruit/RecruitForm.jsx
+++ src/pages/Recruit/RecruitForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Calendar from "../../components/Recruit/Calendar/Calendar";
 import DaumPostcode from "react-daum-postcode";
 import { S, F } from "./style";
@@ -9,6 +9,7 @@ function RecruitForm() {
   const [modalState, setModalState] = useState(false); // 모달 상태 관리
   const [inputAddress, setInputAddress] = useState(""); // 주소 상태 관리
   const [inputZipCode, setInputZipCode] = useState(""); // 우편번호 상태 관리
+  const thumbnailInput = useRef();
 
   const inputText = (event) => {
     setInputValue(event.target.value);
@@ -39,6 +40,35 @@ function RecruitForm() {
   //   setInputZipCode(data.zonecode);
   // };
 
+  const handleClick = () => {
+    thumbnailInput.current.click();
+  };
+
+  // 이미지 업로드
+  const saveFileImage = async (e) => {
+    try {
+      const formData = new FormData(); // formData 생성
+      formData.append("image", e.target.files[0]); // 이미지 파일 값 할당
+
+      const config = {
+        headers: {
+          // Authorization: ...,  // 토큰 넣어주기
+          "Content-Type": "multipart/form-data", // 데이터 형식 지정
+        },
+      };
+      // 이미지 업로드 중
+      const response = await axios.post(`api url`, formData, config); // api 통신
+      // 이미지 업로드 성공
+      dispatch(
+        setWriteContent({
+          type: "thumbnail",
+          value: `${response.data.imageUrl[0]}`,
+        })
+      );
+    } catch (error) {
+      // 이미지 업로드 실패
+    }
+  };
   return (
     <S.Container>
       <S.CardBox>
@@ -47,16 +77,19 @@ function RecruitForm() {
           <F.FormFont>을 입력해주세요</F.FormFont>
         </div>
         <Calendar isRange={true} />
+
         <div>
           <F.ColoredFormFont>신청 마감 일자</F.ColoredFormFont>
           <F.FormFont>을 입력해주세요</F.FormFont>
         </div>
         <Calendar isRange={false} />
+
         <div>
           <F.ColoredFormFont>활동 인원</F.ColoredFormFont>
           <F.FormFont>을 선택해주세요</F.FormFont>
         </div>
         <input value={inputValue} onChange={inputText} />
+
         <div>
           <F.ColoredFormFont>활동 장소</F.ColoredFormFont>
           <F.FormFont>을 선택해주세요</F.FormFont>
@@ -74,14 +107,37 @@ function RecruitForm() {
           <button onClick={() => setModalState(!modalState)}>주소 검색</button>
         </div>
         <DaumPostcode style={postCodeStyle} onComplete={onCompletePost} />
-        <F.CalendarDiv />
-        <div>선택된 주소: {inputAddress}</div>
-        <F.CalendarDiv />
+        <F.CalendarDiv>{inputAddress}</F.CalendarDiv>
+        {/* 상세 주소 넣을 input */}
+        <input></input>
+
         <div>
           <F.ColoredFormFont>제목</F.ColoredFormFont>
           <F.FormFont>을 입력해주세요</F.FormFont>
         </div>
-        <F.CalendarDiv />
+        <input></input>
+
+        <div>
+          <F.ColoredFormFont>내용</F.ColoredFormFont>
+          <F.FormFont>을 입력해주세요</F.FormFont>
+        </div>
+        <textarea placeholder="1000자 이내 작성"></textarea>
+
+        <div>
+          <F.ColoredFormFont>사진</F.ColoredFormFont>
+          <F.FormFont>을 등록해주세요</F.FormFont>
+        </div>
+        <button onClick={handleClick}>
+          사진 첨부
+          <input
+            type="file"
+            accept="image/jpg, image/jpeg, image/png"
+            multiple
+            ref={thumbnailInput}
+            onChange={saveFileImage}
+            style={{ display: "none" }}
+          />
+        </button>
       </S.CardBox>
     </S.Container>
   );
