1deec2260c76fdb447c7cd04ce4cf11641838b4a
parkjoohyung0826 <dochipjh@gmail.com>
2023-12-20 14:51:59 +0900

    feat: login 및 회원정보수정 페이지 완성

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,7 +4,6 @@ import RecruitForm from "./pages/Recruit/RecruitForm";
 import RecruitPost from "./pages/Recruit/RecruitPost";
 import LoginPage from "./pages/login/login"
 import Registration from "./pages/registration/registration";
-import LoginError from "./pages/loginError/loginError";
 import MyV_main from "./pages/myVolunteering/main/main";
 import Modify from "./pages/modify/modify";
 
@@ -15,7 +14,6 @@ export const routes = [
     children: [
       { path: "/modify", element: <Modify /> },
       { path: "/myVolunteer_main", element: <MyV_main /> },
-      { path: "/loginError", element: <LoginError /> },
       { path: "/registration", element: <Registration /> },
       { path: "/recruitHome", element: <LoginPage /> },
       { path: "/recruitForm", element: <RecruitForm /> }, // 팀원모집 페이지
